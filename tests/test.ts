d68fbc822233299a191adcc134924879ad226c76
Jeffrey Mostroso <1376038-jeffrey-omega@users.noreply.gitlab.com>
2023-12-20 12:37:13 +0000

    Refactor test to check for the visibility of a canvas element

--- tests/test.ts
+++ tests/test.ts
@@ -1,19 +1,17 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
-});
-
 test.describe('index page', () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/');
 	});
 
-	test('has expected h1', async ({ page }) => {
+	test('has expected canvas', async ({ page }) => {
 		await page.waitForLoadState('domcontentloaded');
-		
-		await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+
+		// expect the canvas to be visible
+		const canvas = page.locator('canvas');
+		expect(canvas).toBeTruthy();
+
 		// generate a screenshot of the page
 		await page.screenshot({ path: 'static/screenshot.png', fullPage: true });
 	});
