6992954547c702987e28da1ce80dc9cd108ef3b7
Jeffrey Mostroso <1376038-jeffrey-omega@users.noreply.gitlab.com>
2023-12-28 08:57:47 +0100

    Add ThreePerf component for performance profiling

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+	import { DEBUG } from '$lib/Settings';
 	import { Canvas } from '@threlte/core';
 	import Loader from './Loader.svelte';
 	import Scene from './Scene.svelte';
+	import ThreePerf from './ThreePerf.svelte';
 </script>
 
 <Loader />
 
 <Canvas>
+	{#if DEBUG.enablePerformanceProfiling}
+		<ThreePerf />
+	{/if}
 	<Scene />
 </Canvas>
