16e333e9507b05a06a044e247eed67b57f3c6c08
Jeffrey Mostroso <1376038-jeffrey-omega@users.noreply.gitlab.com>
2023-12-28 09:07:18 +0100

    WIP - Update Scene component with new imports and settings

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,50 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+	import { DEBUG } from '$lib/Settings';
+	import { T } from '@threlte/core';
+	import { Gizmo, OrbitControls } from '@threlte/extras';
+	import { DEG2RAD } from 'three/src/math/MathUtils.js';
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
+<T.PerspectiveCamera makeDefault position={[20, 20, 85]} fov={45} near={1}>
+	<!--<OrbitControls
+		autoRotate={true}
+		enableZoom={true}
+		enableDamping
+		autoRotateSpeed={0.5}
+		enablePan={false}
+		maxPolarAngle={85 * DEG2RAD}
+		minPolarAngle={20 * DEG2RAD}
+		maxAzimuthAngle={45 * DEG2RAD}
+		minAzimuthAngle={-45 * DEG2RAD}
+		target.y={1.5}
+	/>-->
+	<OrbitControls
+		autoRotate={true}
+		enableZoom={true}
+		enableDamping
+		autoRotateSpeed={0.5}
+		maxPolarAngle={85 * DEG2RAD}
+		enablePan={false}
+	/>
 </T.PerspectiveCamera>
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
+<!-- Ambient light for ambience -->
+<T.AmbientLight color="#0000ff" intensity={2} />
 
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
-/>
+<!-- Light that casts a shadow -->
+<T.DirectionalLight color="#ffffff" intensity={0.5} castShadow={true} position={[0, 100, 100]} />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
+<!-- Grid helper -->
+{#if DEBUG.enableGridHelper}
+	<T.GridHelper args={[100, 100]} />
+{/if}
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
+<!-- Axes helper -->
+{#if DEBUG.enableAxesHelper}
+	<T.AxesHelper args={[100]} />
+{/if}
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+<!-- Gizmo helper -->
+{#if DEBUG.enableGizmo}
+	<Gizmo horizontalPlacement="left" paddingX={20} paddingY={20} />
+{/if}
